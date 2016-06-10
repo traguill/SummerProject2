3a105fbfa6ad25ab63afc71a542e373fe2953a71
traguill <traguill1@gmail.com>
2016-06-10 11:02:44 +0200

    UnityProject

---
+++ SummerProject2/Assets/Text.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Text : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        print("This is the first commit");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        	
+	}
+}
