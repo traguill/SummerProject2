a0081b8cb82f2bf92ecdbdc7d6f232ba73c00545
crandino <carlosmanbe@hotmail.com>
2016-06-29 13:07:00 +0200

    More camera movement with cursor and numeric keys
    Some border area has been determined to start the camera displacement
    (zone_for_displacement, now at 10%). It was screen size independent, so
    it will adapt to every dimensions we choose. With numeric keys, we start
    a smooth transition, on each player.  There are different smoothness on
    the movement, but they can tested and unified.

--- SummerProject2/Assets/Scripts/CameraMove.cs
+++ SummerProject2/Assets/Scripts/CameraMove.cs
@@ -3,23 +3,91 @@ using System.Collections;
 
 public class CameraMove : MonoBehaviour {
 
-    public float speed_camera;
+    public float speed_camera;   
+    public float zone_for_displacement; // From 0 to 1. For instance, 0.25 means 25% of some lenght (width or height screen dimensions)
+    private Vector2 edge_offset;
+    private Vector3 position_target;
+    private float z_correction; // Z_correction is used for center the different players due to camera inclination.
+    private bool smooth_transition;
 
-	// Use this for initialization
-	void Start ()
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+
+    // Use this for initialization
+    void Start ()
     {
         speed_camera = 10;
-	}
+        zone_for_displacement = 0.1f;   // 10%
+        edge_offset.Set(zone_for_displacement * Screen.width, zone_for_displacement * Screen.height);
+        z_correction = transform.position.y / Mathf.Tan(transform.eulerAngles.x * Mathf.PI / 180);
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        float h_dis = Input.GetAxis("CameraHorizontal");
-        float v_dis = Input.GetAxis("CameraVertical");
+        checkingCameraMovement();
+        if (smooth_transition)
+            moveCamera();
+    }
+
+    void checkingCameraMovement()
+    {
+        Vector3 mouse_position = Input.mousePosition;
         Vector3 pos = transform.position;
-        Vector3 new_pos = transform.position;
-        
-        new_pos.Set(pos.x + (h_dis * speed_camera * Time.deltaTime), pos.y, pos.z + (v_dis * speed_camera * Time.deltaTime));
-        transform.position = new_pos;
+
+        if (mouse_position.x < edge_offset.x)
+            pos.Set(pos.x + (-speed_camera * Time.deltaTime), pos.y, pos.z);
+        else if (mouse_position.x > Screen.width - edge_offset.x)
+            pos.Set(pos.x + (speed_camera * Time.deltaTime), pos.y, pos.z);
+
+        if (mouse_position.y < edge_offset.y)
+            pos.Set(pos.x, pos.y, pos.z + (-speed_camera * Time.deltaTime));
+        else if (mouse_position.y > Screen.height - edge_offset.y)
+            pos.Set(pos.x, pos.y, pos.z + (speed_camera * Time.deltaTime));
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            float h_dis = Input.GetAxis("CameraHorizontal");
+            float v_dis = Input.GetAxis("CameraVertical");
+            pos.Set(pos.x + (h_dis * speed_camera * Time.deltaTime), pos.y, pos.z + (v_dis * speed_camera * Time.deltaTime));
+        }
+
+        // Updating position
+        transform.position = pos;
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            smooth_transition = true;
+            position_target.Set(player1.transform.position.x, pos.y, player1.transform.position.z - z_correction);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            smooth_transition = true;
+            position_target.Set(player2.transform.position.x, pos.y, player2.transform.position.z - z_correction);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            smooth_transition = true;
+            position_target.Set(player3.transform.position.x, pos.y, player3.transform.position.z - z_correction);
+        }
+
+        //if (Input.GetKey(KeyCode.Alpha3))
+        //{
+        //    smooth_transition = true;
+        //    position_target.Set(player4.transform.position.x, pos.y, player4.transform.position.z - z_correction);
+        //}
+    }
+
+    void moveCamera()
+    {
+        transform.position = Vector3.Lerp(transform.position, position_target, 0.5f);
+        float minimun_distance = 0.1f;
+
+        if (Vector3.Distance(transform.position, position_target) < minimun_distance)
+            smooth_transition = false;
     }
 }
