ea822ab32f058a35fec3c03d00a421efff9380a6
crandino <carlosmanbe@hotmail.com>
2016-06-29 11:39:37 +0200

    Camera movement with Arrow keys
    On InputManager, we can tweak this smooth movement to something more
    straight forward.

---
+++ SummerProject2/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    public float speed_camera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        speed_camera = 10;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float h_dis = Input.GetAxis("CameraHorizontal");
+        float v_dis = Input.GetAxis("CameraVertical");
+        Vector3 pos = transform.position;
+        Vector3 new_pos = transform.position;
+        
+        new_pos.Set(pos.x + (h_dis * speed_camera * Time.deltaTime), pos.y, pos.z + (v_dis * speed_camera * Time.deltaTime));
+        transform.position = new_pos;
+    }
+}
