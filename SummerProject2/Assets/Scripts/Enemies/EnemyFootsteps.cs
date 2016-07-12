b65726dce32ead0ca946d1beeed27f30e0ba0038
traguill <traguill1@gmail.com>
2016-07-12 10:57:40 +0200

    Enemy footsteps

---
+++ SummerProject2/Assets/Scripts/Enemies/EnemyFootsteps.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFootsteps : MonoBehaviour
+{
+    GameObject enemy;
+    ParticleSystem footsteps;
+
+	void Start ()
+    {
+        enemy = transform.parent.gameObject;
+        footsteps = GetComponent<ParticleSystem>();
+
+        footsteps.Stop();
+	}
+	
+	void Update ()
+    {
+        //Check if enemy is visible
+        if(enemy.GetComponent<SpriteRenderer>().enabled == true)
+        {
+            if (footsteps.isPlaying)
+                footsteps.Stop();
+        }
+        else
+        {
+            if (footsteps.isPlaying == false)
+                footsteps.Play();
+        }
+	
+	}
+}
