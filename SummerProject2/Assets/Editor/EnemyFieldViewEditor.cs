aebab7c42290e68764db3f3deaae42d2b8b149dc
traguill <traguill1@gmail.com>
2016-07-13 10:15:49 +0200

    EnemyFieldView

---
+++ SummerProject2/Assets/Editor/EnemyFieldViewEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(EnemyFieldView))]
+public class EnemyFieldViewEditor : Editor {
+
+	void OnSceneGUI()
+    {
+        EnemyFieldView item = (EnemyFieldView)target;
+
+        Handles.color = Color.green;
+        Handles.DrawWireArc(item.transform.position, Vector3.up, Vector3.forward, 360, item.view_radius);
+        Vector3 view_angle_A = item.DirectionFromAngle(-item.view_angle / 2, false);
+        Vector3 view_angle_B = item.DirectionFromAngle(item.view_angle / 2, false);
+
+        Handles.DrawLine(item.transform.position, item.transform.position + view_angle_A * item.view_radius);
+        Handles.DrawLine(item.transform.position, item.transform.position + view_angle_B * item.view_radius);
+
+        Handles.color = Color.red;
+
+        foreach (Transform visible_target in item.visible_targets)
+        {
+            Handles.DrawLine(item.transform.position, visible_target.position);
+        }
+
+    }
+}
---
+++ SummerProject2/Assets/Scripts/Enemies/EnemyFieldView.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyFieldView : MonoBehaviour {
+
+    public float view_radius;
+    [Range(0, 360)]
+    public float view_angle;
+
+    public LayerMask target_mask;
+    public LayerMask obstacle_mask;
+
+    [HideInInspector]
+    public List<Transform> visible_targets = new List<Transform>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine("FindTargetsWithDelay", 0.2f);
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visible_targets.Clear();
+        Collider[] targets_in_view_radius = Physics.OverlapSphere(transform.position, view_radius, target_mask);
+
+        for (int i = 0; i < targets_in_view_radius.Length; i++)
+        {
+            Transform target = targets_in_view_radius[i].transform;
+            Vector3 direction = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, direction) < view_angle / 2)
+            {
+                float distance_to_target = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, direction, distance_to_target, obstacle_mask))
+                {
+                    visible_targets.Add(target);
+                }
+            }
+        }
+    }
+
+    public Vector3 DirectionFromAngle(float angle, bool angle_is_global)
+    {
+        if (!angle_is_global)
+        {
+            angle += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+    }
+}
