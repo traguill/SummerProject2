1a5c3f9dc5e1c72b3390aa5ba034748dfb2988b5
traguill <traguill1@gmail.com>
2016-07-15 11:00:44 +0200

    Mentalist Player finished & Updated selection system

---
+++ SummerProject2/Assets/Scripts/Players/Assassin/AssassinController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssassinController : MonoBehaviour {
+
+    enum State
+    {
+        IDLE,
+        WALKING,
+        WALKING_TO_KILL, //Approaching to selected enemy to kill it
+        KILLING //Action to kill
+    };
+
+    State state;
+
+	void Start ()
+    {
+        state = State.IDLE;
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
--- SummerProject2/Assets/Scripts/Players/Mentalist/MentalistController.cs
+++ SummerProject2/Assets/Scripts/Players/Mentalist/MentalistController.cs
@@ -12,16 +12,58 @@ public class MentalistController : MonoBehaviour {
 
     State state;
 
+    //Selection
+    public GameObject selection_circle;
+    UnitSelection selection_system;
+    bool is_selected = false;
+
+    //Navigation
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        selection_system = GameObject.Find("SelectionSystem").GetComponent<UnitSelection>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+
 	// Use this for initialization
 	void Start ()
     {
         state = State.IDLE;
+        
+        selection_circle.SetActive(false);
+        is_selected = false;
 	}
 	
 	void Update ()
     {
+        UpdateSelection();
+
         AbilitiesInput();
+
+        switch(state)
+        {
+            case State.IDLE:
+                InputMouse();
+                break;
+            case State.WALKING:
+                InputMouse();
+                break;
+            case State.SENSORIAL:
+                break;
+        }
 	}
+
+    /// <summary>
+    /// Checks if the Player is selected and updates the selection circle.
+    /// </summary>
+    private void UpdateSelection()
+    {
+        is_selected = selection_system.IsPlayerSelected(gameObject);
+
+        selection_circle.SetActive(is_selected);
+    }
 
     private void AbilitiesInput()
     {
@@ -30,9 +72,37 @@ public class MentalistController : MonoBehaviour {
             if(GetComponent<SensorialAbility>().is_playing == false)
             {
                 GetComponent<SensorialAbility>().UseAbility();
-
-            }
-           
+            }           
         }
+    }
+
+    /// <summary>
+    /// Handles all the logic of a mouse input (movement, objectives, objects, etc).
+    /// </summary>
+    private void InputMouse()
+    {
+        //Right click UP
+        if(Input.GetMouseButtonUp(1))
+        {
+            //Check what is under the mouse (enemy, object, nothing...)
+
+            //For now only movment
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.SetDestination(hit.point);
+                state = State.WALKING;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// stopMovement finishes all pathFinding activity
+    /// </summary>
+    public void stopMovement()
+    {
+        agent.Stop();
+        agent.ResetPath();
     }
 }
