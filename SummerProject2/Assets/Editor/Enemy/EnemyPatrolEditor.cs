354e3e69a6182872c891afa289d363a01d0d4b06
crandino <carlosmanbe@hotmail.com>
2016-07-16 17:03:12 +0200

    Experiments with Editor
    On Scene View, the patrol of the enemy is shown. The patrol path is a
    loop, but the code doesn't represent it. I will change that later! Don't
    worry.

---
+++ SummerProject2/Assets/Editor/Enemy/EnemyPatrolEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(EnemyPatrol))]
+public class EnemyPatrolEditor : Editor
+{
+    [ExecuteInEditMode]
+    void OnSceneGUI()
+    {
+        EnemyPatrol item = (EnemyPatrol)target;
+        Transform[] path = item.assigned_path.transform.getChilds();
+
+        Vector3[] lineSegments = new Vector3[path.Length * 2];
+        int pointIndex = 0;
+
+        for (int i = 0; i < path.Length - 1; i++)
+        {
+            lineSegments[pointIndex++] = path[i].transform.position;
+            lineSegments[pointIndex++] = path[i + 1].transform.position;
+        }
+
+        Handles.DrawDottedLines(lineSegments, 1);
+
+        //Vector3 view_angle_A = item.DirectionFromAngle(-item.view_angle / 2, false);
+        //Vector3 view_angle_B = item.DirectionFromAngle(item.view_angle / 2, false);
+
+        //Handles.DrawLine(item.transform.position, item.transform.position + view_angle_A * item.view_radius);
+        //Handles.DrawLine(item.transform.position, item.transform.position + view_angle_B * item.view_radius);
+
+        //Handles.color = Color.red;
+
+        //foreach (Transform visible_target in item.visible_targets)
+        //{
+        //    Handles.DrawLine(item.transform.position, visible_target.position);
+        //}
+
+    }
+
+
+}
