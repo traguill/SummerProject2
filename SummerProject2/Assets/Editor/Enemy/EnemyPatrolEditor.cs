af964bfe8247f1bc0048219cf5936735e3ac9ed7
crandino <carlosmanbe@hotmail.com>
2016-07-16 17:03:44 +0200

    GitHub is a :turtle:

--- SummerProject2/Assets/Editor/Enemy/EnemyPatrolEditor.cs
+++ SummerProject2/Assets/Editor/Enemy/EnemyPatrolEditor.cs
@@ -22,20 +22,5 @@ public class EnemyPatrolEditor : Editor
 
         Handles.DrawDottedLines(lineSegments, 1);
 
-        //Vector3 view_angle_A = item.DirectionFromAngle(-item.view_angle / 2, false);
-        //Vector3 view_angle_B = item.DirectionFromAngle(item.view_angle / 2, false);
-
-        //Handles.DrawLine(item.transform.position, item.transform.position + view_angle_A * item.view_radius);
-        //Handles.DrawLine(item.transform.position, item.transform.position + view_angle_B * item.view_radius);
-
-        //Handles.color = Color.red;
-
-        //foreach (Transform visible_target in item.visible_targets)
-        //{
-        //    Handles.DrawLine(item.transform.position, visible_target.position);
-        //}
-
     }
-
-
 }
