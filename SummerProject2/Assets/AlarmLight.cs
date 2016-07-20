bdfaf489ca904d18752e8277d7e9aa9f9c87f999
crandino <carlosmanbe@hotmail.com>
2016-07-20 17:26:14 +0200

    Alarm Light

---
+++ SummerProject2/Assets/AlarmLight.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour
+{
+    public float fade_speed = 2f;            // How fast the light fades between intensities.
+    public float high_intensity = 2f;        // The maximum intensity of the light whilst the alarm is on.
+    public float low_intensity = 0.5f;       // The minimum intensity of the light whilst the alarm is on.
+    public float change_margin = 0.2f;       // The margin within which the target intensity is changed.
+    public bool  alarm_on;                   // Whether or not the alarm is on.
+
+    private Light alarm_light;              // The light GameObject that will be modified.
+    private float targetIntensity;          // The intensity that the light is aiming for currently.
+
+    void Awake()
+    {
+        // Getting light from Alarm component
+        alarm_light = GetComponent<Light>();
+        // When the level starts we want the light to be "off".
+        alarm_light.intensity = 0f;
+        // When the alarm starts for the first time, the light should aim to have the maximum intensity.
+        targetIntensity = high_intensity;
+    }
+
+
+    void Update()
+    {
+        // If the light is on...
+        if (alarm_on)
+        {
+            // ... Lerp the light's intensity towards the current target.
+            alarm_light.intensity = Mathf.Lerp(alarm_light.intensity, targetIntensity, fade_speed * Time.deltaTime);
+
+            // Check whether the target intensity needs changing and change it if so.
+            CheckTargetIntensity();
+        }
+        else
+            // Otherwise fade the light's intensity to zero.
+            alarm_light.intensity = Mathf.Lerp(alarm_light.intensity, 0f, fade_speed * Time.deltaTime);
+    }
+
+
+    void CheckTargetIntensity()
+    {
+        // If the difference between the target and current intensities is less than the change margin...
+        if (Mathf.Abs(targetIntensity - alarm_light.intensity) < change_margin)
+        {
+            // ... if the target intensity is high...
+            if (targetIntensity == high_intensity)
+                // ... then set the target to low.
+                targetIntensity = low_intensity;
+            else
+                // Otherwise set the targer to high.
+                targetIntensity = high_intensity;
+        }
+    }
+}
