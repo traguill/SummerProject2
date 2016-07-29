66004a6716ff744f994e8c41835e2d68d24dabd8
crandino <carlosmanbe@hotmail.com>
2016-07-29 11:48:40 +0200

    Security Camera detection

--- SummerProject2/Assets/CameraDetection.cs
+++ SummerProject2/Assets/CameraDetection.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class CameraDetection : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private AlarmSystem alarm_system;
+
+    void Awake()
+    {
+        alarm_system = GameObject.FindGameObjectWithTag(Tags.game_controller).GetComponent<AlarmSystem>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Tags.player))
+            alarm_system.SetAlarm(ALARM_STATE.ALARM_ON);
+    }
 }
