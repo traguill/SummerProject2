391a7a563ff02cf8444c1c3c538ea7ecb34d1937
traguill <traguill1@gmail.com>
2016-08-04 20:47:46 +0200

    HUD sprites integrated in game

--- SummerProject2/Assets/Scripts/UI/HUDAbility.cs
+++ SummerProject2/Assets/Scripts/UI/HUDAbility.cs
@@ -7,18 +7,47 @@ public class HUDAbility : MonoBehaviour
 
     public Cooldown controller; //Handles the cooldown of the ability. Here just ask for info.
     public int id; //Id of the ability
-    public Image cooldown_clock;  //Clock of the cooldown.
+    Image cooldown_clock;  //Clock of the cooldown.
 
     bool cooldown_active = false; //Is the ability in cooldown
+
+    public HUD_Character character; //Character reference to handle selection
+
+    public Sprite selected_sprite;
+    public Sprite unselected_sprite;
+
+    bool selected = false; //Is the character selected
+
+    void Awake()
+    {
+        cooldown_clock = GetComponent<Image>();
+    }
 	
 	void Update ()
     {
+        //Update selection animation
+        if(character.selected != selected) //With this avoids setting the sprite every frame
+        {
+            if (character.selected == true) //Selected
+            {
+                cooldown_clock.sprite = selected_sprite;
+                selected = true;
+            }
+            else //Unselected
+            {
+                cooldown_clock.sprite = unselected_sprite;
+                selected = false;
+            }
+        }
+        
+
+        //Update cooldown animation
         if(cooldown_active == false)
         {
             if (controller.abilities[id].in_use == true) //Cooldown started
             {
                 cooldown_active = true;
-                cooldown_clock.fillAmount = 1;
+                cooldown_clock.fillAmount = 0;
             }
                 
         }
@@ -26,12 +55,12 @@ public class HUDAbility : MonoBehaviour
         {
             //Update clock animation
             float clock_value = controller.abilities[id].timer / controller.abilities[id].cooldown_seconds;
-            cooldown_clock.fillAmount = 1 - clock_value;
+            cooldown_clock.fillAmount = clock_value;
 
             if (controller.abilities[id].in_use == false) //Cooldown finished
             {
                 cooldown_active = false;
-                cooldown_clock.fillAmount = 0;
+                cooldown_clock.fillAmount = 1;
             }
         }
         
--- SummerProject2/Assets/Scripts/UI/HUD_Character.cs
+++ SummerProject2/Assets/Scripts/UI/HUD_Character.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using System;
 
 public class HUD_Character : MonoBehaviour, IPointerClickHandler
@@ -9,9 +10,35 @@ public class HUD_Character : MonoBehaviour, IPointerClickHandler
 
     public GameObject character;
 
+    public GameObject selected_sprite;
+    public GameObject unselected_sprite;
+
+    [HideInInspector] public bool selected = false;
+
     void Awake()
     {
         selection_system = GameObject.Find("SelectionSystem").GetComponent<UnitSelection>();
+    }
+
+    void Update()
+    {
+        bool is_selected = selection_system.IsPlayerSelected(character);
+
+        if(is_selected != selected) //Selection has changed (update sprite)
+        {
+            if(is_selected == false)
+            {
+                unselected_sprite.SetActive(true);
+                selected_sprite.SetActive(false);
+                selected = false;
+            }
+            else
+            {
+                selected_sprite.SetActive(true);
+                unselected_sprite.SetActive(false);
+                selected = true;
+            }
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
