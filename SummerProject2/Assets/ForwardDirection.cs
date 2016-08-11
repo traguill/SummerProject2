06c24e5a0d31c844ccb2052035c14562b745d896
crandino <carlosmanbe@hotmail.com>
2016-08-11 19:24:58 +0200

    Arrow showing cone of vision direction

---
+++ SummerProject2/Assets/ForwardDirection.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForwardDirection : MonoBehaviour {
+
+    private RhandorController rhandor;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        rhandor = GetComponentInParent<RhandorController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(rhandor.transform.position.x, rhandor.transform.position.y, rhandor.transform.position.z) ;
+        
+	}
+}
