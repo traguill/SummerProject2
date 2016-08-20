55b6b32addcf0cd92811ec9ae7cb9ba2af1d2379
traguill <traguill1@gmail.com>
2016-08-20 17:16:43 +0200

    Implemented Bullet-time (left-shift enable/disable)

---
+++ SummerProject2/Assets/Scripts/Systems/BulletTime/BulletTime.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletTime : MonoBehaviour {
+
+    public float amount_slow = 0.5f;
+
+    bool enabled = false;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            enabled = !enabled;
+
+            if (enabled == true)
+                Time.timeScale = amount_slow;
+            else
+                Time.timeScale = 1.0f; //Normal time scale
+        }
+	
+	}
+}
