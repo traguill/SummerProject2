e21b8a109fb724ca8363d0723252d8ce8b86d739
crandino <carlosmanbe@hotmail.com>
2016-09-06 21:27:33 +0200

    Scrolling with mouse
    With the center button of mouse, the player can recover its initial camera position

--- SummerProject2/Assets/Scripts/ExtensionMethods/ExtensionMethods.cs
+++ SummerProject2/Assets/Scripts/ExtensionMethods/ExtensionMethods.cs
@@ -29,6 +29,23 @@ public static class ExtensionMethods
         return ret;
     }
 
+    public static void SetPosition(this Transform trans, Vector3 new_pos)
+    {
+        trans.position = new_pos;
+    }
+
+    public static void UpdatePosition(this Transform trans, float x, float y, float z)
+    {
+        trans.position += new Vector3(x,y,z);
+    }
+
+    /// <summary>
+    /// Determines if a vector is close to another vector within a delta threshold
+    /// </summary>
+    /// <param name="our_vector"></param>
+    /// <param name="vec_to_compare"></param>
+    /// <param name="delta"></param>
+    /// <returns></returns>
     public static bool IsCloseTo(this Vector3 our_vector, Vector3 vec_to_compare, float delta)
     {
         return (Mathf.Abs(our_vector.x - vec_to_compare.x) < delta && 
