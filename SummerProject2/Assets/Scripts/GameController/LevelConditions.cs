6c4967e5f25500daefac8eb8749a394e4646f1d8
traguill <traguill1@gmail.com>
2016-09-08 16:16:28 +0200

    Checkpoint system

--- SummerProject2/Assets/Scripts/GameController/LevelConditions.cs
+++ SummerProject2/Assets/Scripts/GameController/LevelConditions.cs
@@ -9,6 +9,8 @@ public class LevelConditions : MonoBehaviour
     private float radius;
     public static int players_at_end_level;
 
+    CheckPointManager checkpoint_manager;
+
     // Call before any Start function
     void Awake()
     {
@@ -18,11 +20,16 @@ public class LevelConditions : MonoBehaviour
         start_point = GameObject.Find("Start_point").transform;   // I don't know any other efficient way!
         radius = 3.5f;
         players_at_end_level = 0;
+
+
+        checkpoint_manager = GameObject.Find("CheckPointSystem").GetComponent<CheckPointManager>();
     }
 
     // Use this for initialization
     void Start()
     {
+        checkpoint_manager.InitializeCheckpoints(start_point); //Get the checkpoint to spawn in the right position
+
         // The different players is distributed equidistantly on a circle of radius "radius". The script automatically take into account
         // the number of players to put along the circle.
         float angle = 0.0f;
---
+++ SummerProject2/Assets/Scripts/Systems/CheckPoints/CheckPointManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CheckPointManager : MonoBehaviour {
+
+
+    private static bool created = false;
+
+    public List<CheckPoint> checkpoints;
+
+    int checkpoint_id = 0; //Current progress of the checkpoint
+
+    Vector3 spawn_position = new Vector3();
+
+    void Awake()
+    {
+        if(!created)
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            created = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        checkpoint_id = 0;
+    }
+
+    /// <summary>
+    /// Called when the level is loaded again or for the first time
+    /// </summary>
+    public void InitializeCheckpoints(Transform start_position)
+    {
+
+        //Set manager reference
+        foreach(CheckPoint point in checkpoints)
+        {
+            point.InitializeManager(this);
+        }
+
+        //Activate the next checkpoint
+        if(checkpoint_id < checkpoints.Count)
+            checkpoints[checkpoint_id].active = true; 
+
+        //First checkpoint passed
+        if(checkpoint_id != 0)
+        {
+            spawn_position = checkpoints[checkpoint_id - 1].GetSpawnPosition();
+            start_position.position = spawn_position;
+        }
+        
+    }
+
+    public void NextCheckPoint()
+    {
+        checkpoints[checkpoint_id].active = false;
+        checkpoint_id += 1;
+        if (checkpoint_id < checkpoints.Count)
+        {
+            checkpoints[checkpoint_id].active = true;
+        }
+    }
+
+}
